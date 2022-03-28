16fdfdbc2deeb394452ab669373d8be816686ce4
Alina Rallo <alina.korzik95@mail.ru>
2022-03-28 11:58:05 +0300

    env

--- resizer.js
+++ resizer.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
 const { includes } = require('lodash');
+require('dotenv').config()
 
-const imageSize = process.argv[2];
+const imageSize = process.env.IMAGE_SIZE;
 const width = Number(imageSize.split('x')[0]);
 const height = Number(imageSize.split('x')[1]);
-const dirInputPath = process.argv[3];
-const dirOutputPath = process.argv[4];
+const dirInputPath = process.env.DIR_INPUT_PATH;
+const dirOutputPath = process.env.DIR_OUTPUT_PATH;
 
 const readInputDir = fs.readdirSync(dirInputPath);
 const readOutputDir = fs.readdirSync(dirOutputPath);
